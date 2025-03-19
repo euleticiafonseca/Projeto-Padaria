0fb13c3d0866014b93959726d76cf9768bac283e
im.leticiafonseca <leticia.fsbento@senacsp.edu.br>
2025-03-19 11:51:07 -0300

    Projeto Padaria

---
+++ Padaria/frmLogin.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Padaria
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmLogin));
+            this.pctLogo = new System.Windows.Forms.PictureBox();
+            this.lblUsuario = new System.Windows.Forms.Label();
+            this.lblSenha = new System.Windows.Forms.Label();
+            this.btnEntrar = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.txtSenha = new System.Windows.Forms.TextBox();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pctLogo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pctLogo
+            // 
+            resources.ApplyResources(this.pctLogo, "pctLogo");
+            this.pctLogo.Name = "pctLogo";
+            this.pctLogo.TabStop = false;
+            // 
+            // lblUsuario
+            // 
+            resources.ApplyResources(this.lblUsuario, "lblUsuario");
+            this.lblUsuario.Name = "lblUsuario";
+            // 
+            // lblSenha
+            // 
+            resources.ApplyResources(this.lblSenha, "lblSenha");
+            this.lblSenha.Name = "lblSenha";
+            this.lblSenha.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // btnEntrar
+            // 
+            resources.ApplyResources(this.btnEntrar, "btnEntrar");
+            this.btnEntrar.Name = "btnEntrar";
+            this.btnEntrar.UseVisualStyleBackColor = true;
+            // 
+            // btnSair
+            // 
+            resources.ApplyResources(this.btnSair, "btnSair");
+            this.btnSair.Name = "btnSair";
+            this.btnSair.UseVisualStyleBackColor = true;
+            // 
+            // txtSenha
+            // 
+            resources.ApplyResources(this.txtSenha, "txtSenha");
+            this.txtSenha.Name = "txtSenha";
+            // 
+            // txtUsuario
+            // 
+            resources.ApplyResources(this.txtUsuario, "txtUsuario");
+            this.txtUsuario.Name = "txtUsuario";
+            // 
+            // frmLogin
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.txtUsuario);
+            this.Controls.Add(this.txtSenha);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnEntrar);
+            this.Controls.Add(this.lblSenha);
+            this.Controls.Add(this.lblUsuario);
+            this.Controls.Add(this.pctLogo);
+            this.Name = "frmLogin";
+            ((System.ComponentModel.ISupportInitialize)(this.pctLogo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pctLogo;
+        private System.Windows.Forms.Label lblUsuario;
+        private System.Windows.Forms.Label lblSenha;
+        private System.Windows.Forms.Button btnEntrar;
+        private System.Windows.Forms.Button btnSair;
+        private System.Windows.Forms.TextBox txtSenha;
+        private System.Windows.Forms.TextBox txtUsuario;
+    }
+}
---
+++ Padaria/frmSplash.Designer.cs
@@ -0,0 +1,111 @@
+﻿namespace Padaria
+{
+    partial class frmSplash
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmSplash));
+            this.pcbLogo = new System.Windows.Forms.PictureBox();
+            this.lblNumero = new System.Windows.Forms.Label();
+            this.lblPorcentagem = new System.Windows.Forms.Label();
+            this.pgbLogo = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbLogo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pcbLogo
+            // 
+            this.pcbLogo.Image = ((System.Drawing.Image)(resources.GetObject("pcbLogo.Image")));
+            this.pcbLogo.Location = new System.Drawing.Point(258, 137);
+            this.pcbLogo.Name = "pcbLogo";
+            this.pcbLogo.Size = new System.Drawing.Size(222, 135);
+            this.pcbLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pcbLogo.TabIndex = 0;
+            this.pcbLogo.TabStop = false;
+            this.pcbLogo.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // lblNumero
+            // 
+            this.lblNumero.AutoSize = true;
+            this.lblNumero.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumero.Location = new System.Drawing.Point(357, 321);
+            this.lblNumero.Name = "lblNumero";
+            this.lblNumero.Size = new System.Drawing.Size(18, 20);
+            this.lblNumero.TabIndex = 1;
+            this.lblNumero.Text = "0";
+            this.lblNumero.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblPorcentagem
+            // 
+            this.lblPorcentagem.AutoSize = true;
+            this.lblPorcentagem.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPorcentagem.Location = new System.Drawing.Point(371, 321);
+            this.lblPorcentagem.Name = "lblPorcentagem";
+            this.lblPorcentagem.Size = new System.Drawing.Size(23, 20);
+            this.lblPorcentagem.TabIndex = 2;
+            this.lblPorcentagem.Text = "%";
+            this.lblPorcentagem.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // pgbLogo
+            // 
+            this.pgbLogo.ForeColor = System.Drawing.SystemColors.MenuHighlight;
+            this.pgbLogo.Location = new System.Drawing.Point(258, 363);
+            this.pgbLogo.Name = "pgbLogo";
+            this.pgbLogo.Size = new System.Drawing.Size(222, 23);
+            this.pgbLogo.TabIndex = 3;
+            this.pgbLogo.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // frmSplash
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(780, 557);
+            this.Controls.Add(this.pgbLogo);
+            this.Controls.Add(this.lblPorcentagem);
+            this.Controls.Add(this.lblNumero);
+            this.Controls.Add(this.pcbLogo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "frmSplash";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Padaria - Splash";
+            this.Load += new System.EventHandler(this.frmSplash_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pcbLogo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pcbLogo;
+        private System.Windows.Forms.Label lblNumero;
+        private System.Windows.Forms.Label lblPorcentagem;
+        private System.Windows.Forms.ProgressBar pgbLogo;
+    }
+}
+
